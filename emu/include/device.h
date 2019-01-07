f08a0eccddb5ee347b5e2898df87c5313d0bc554
Benjamin O'Neill <benjaminmoneill@gmail.com>
2019-01-07 16:36:22 -0500

    Update display function

--- emu/include/device.h
+++ emu/include/device.h
@@ -6,22 +6,23 @@
 #include <SDL2/SDL.h>
 
 #define SDL_WINDOW_SIZE 512
-#define DISPLAY1_SIZE 64
+#define DISPLAY1_SIZE 256
 
 typedef struct display1 {
-    bool p[DISPLAY1_SIZE * DISPLAY1_SIZE];
-    int o[DISPLAY1_SIZE * DISPLAY1_SIZE];
+    bool p[DISPLAY1_SIZE][DISPLAY1_SIZE];
     bool w;
     int i;
+    int x, y;
     SDL_Window *window;
     SDL_Renderer *renderer;
 } display1_t;
 
 static display1_t *display;
 
-void init_display1();
 static SDL_Rect *r;
 static SDL_Rect *fillr;
-void write_display1(word imm);
+
+void init_display1();
+void write_display1(word imm, cpu_t *cpu);
 
 #endif // EMU_DEVICE_H
